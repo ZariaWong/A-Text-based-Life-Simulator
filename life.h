1337ae5b88696bff1518b5f0f2c7286340811775
WADIDIDI <91597322+WADIDIDI@users.noreply.github.com>
2021-11-30 21:28:15 +0800

    Create life.h

---
+++ life.h
@@ -0,0 +1,18 @@
+// life.h
+#ifdef LIFE_H
+#define LIFE_H
+
+int random(int min, int y);
+Void Playing_with_friends();
+Void Watching_TV();
+Void Reading();
+Void Cultivating_hobbies();
+Void Playing_computer();
+Void Studying();
+Void Writing();
+Void Internship();
+Void Competition();
+Void Hall_activities();
+Void Parties();
+Void bad_performance();
+#endif
