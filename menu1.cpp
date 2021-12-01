8ca4b3febc41278e2c437d00a5e0c0e25deea20e
WADIDIDI <91597322+WADIDIDI@users.noreply.github.com>
2021-12-01 21:57:40 +0800

    Update menu1.cpp

--- menu1.cpp
+++ menu1.cpp
@@ -1,2 +1,36 @@
 #include <iostream>
 using namespace std;
+
+void start(){
+  
+}
+
+void save(){
+  
+}
+
+void restart(){
+  
+}
+
+void exit(){
+  cout << "Exiting. Welcome to continue next time!" << endl;
+  
+}
+
+void DrawMenu(){
+  cout << " ☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆" << endl;
+	cout << "|                      Life Simulation                      |" << endl;
+  cout << "|                                                           |" << endl;
+  cout << "|                           Start                           |" << endl;
+  cout << "|                                                           |" << endl;
+  cout << "|                           Save                            |" << endl;
+	cout << "|                                                           |" << endl;
+  cout << "|                          Restart                          |" << endl;
+  cout << "|                                                           |" << endl;
+	cout << "|                           Exit                            |" << endl;
+  cout << "|                                                           |" << endl;
+  cout << " ☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆-☆" << endl;
+	cout << "                                      Designer: Zaria, Wadi" << endl;
+		
+}
