37939e6c742648e7d6437bc1793c42546ee27a86
WADIDIDI <91597322+WADIDIDI@users.noreply.github.com>
2021-12-02 13:21:57 +0800

    Update menu1.cpp

--- menu1.cpp
+++ menu1.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-void start(){
-  
-}
-
+// write to file (save)
 void save(){
+  ofstream fout;
+  fout.open("output.txt");
+  if (fout.fail()){
+    cout << "Error in file openning!" << endl;
+    exit(1);
+  }
+  fout << name << endl;
+  fout.close();
+	
   cout << "*******************" << endl;
   cout << "Successfully saved!" << endl;
   cout << "*******************" << endl;
+}
+
+
+// read from file (load/ start)
+void start(){
+  ifstream fin;
+  fin.open("output.txt");
+  if (fin.fail()){
+    cout << "Error in file opening!" << endl;
+    exit(1);
+  }
+  fin >> 
+  fin.close();
 }
 
 void restart(){
